abe186330726c302cc582919fcbb548cad064ed3
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-08-17 02:38:30 +0900

    Add utils module

---
+++ xarray_compare/utils.py
@@ -0,0 +1,24 @@
+__all__ = ["dataarray_method"]
+
+
+# standard library
+from functools import wraps
+from typing import Callable
+
+
+# dependencies
+from xarray import register_dataarray_accessor
+
+
+# main features
+def dataarray_method(func: Callable) -> Callable:
+    class Accessor:
+        def __init__(self, dataarray):
+            self.dataarray = dataarray
+
+        @wraps(func)
+        def __call__(self, *args, **kwargs):
+            return func(self.dataarray, *args, **kwargs)
+
+    register_dataarray_accessor(func.__name__)(Accessor)
+    return func

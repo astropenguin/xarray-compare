44b3fa4dcfc4b2685b1d56d707a0c533229cf1c3
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-08-17 19:47:54 +0900

    Update docstring

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,7 +5,7 @@ from xarray_compare.utils import dataarray_method
 
 # test functions
 def test_dataarray_method():
-    """Whether a decorated function can also be used as a method."""
+    """Test whether a decorated function can also be used as a method."""
 
     @dataarray_method
     def test_function(dataarray):

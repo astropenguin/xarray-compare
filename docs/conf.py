4cb0edc0e4248cc61ba6fa6c729fb85d70c980c6
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-08-20 13:42:44 +0900

    Add sphinx docs

---
+++ docs/conf.py
@@ -0,0 +1,31 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("."))
+
+
+# Project information
+project = "xarray-compare"
+copyright = "2020, Akio Taniguchi"
+author = "Akio Taniguchi"
+release = "0.1.1"
+
+
+# General configuration
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+]
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# Options for HTML output
+html_static_path = ["_static"]
+html_logo = "_static/logo.svg"
+html_theme = "pydata_sphinx_theme"
+html_theme_options = {
+    "github_url": "https://github.com/astropenguin/xarray-compare/",
+    "twitter_url": "https://twitter.com/astropengu_in/",
+}
---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. xarray-compare documentation master file, created by
+   sphinx-quickstart on Tue Aug 18 14:53:06 2020.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to xarray-compare's documentation!
+==========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`

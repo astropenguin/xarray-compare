1f2afb594b9130a1d57cb00b77e5f6b19a231f40
Akio Taniguchi <taniguchi@a.phys.nagoya-u.ac.jp>
2020-08-20 15:41:11 +0900

    Update package version (0.1.0 → 0.1.1)

--- tests/test_package.py
+++ tests/test_package.py
@@ -4,7 +4,7 @@ from xarray_compare import __author__, __version__
 
 # test functions
 def test_version():
-    assert __version__ == "0.1.0"
+    assert __version__ == "0.1.1"
 
 
 def test_author():
